d62434a376e3c849e6fffd33b9c4d3dbed2fc13f
Gaith-M <mhdgaith94@gmail.com>
2020-04-25 11:43:40 +0300

    Updates

---
+++ precache-manifest.668cd936b1bba2baa0f550df45eb1176.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b316138ab772a6c84d064a1bc0249a6",
+    "url": "/css-gradient-generator/index.html"
+  },
+  {
+    "revision": "f8b3eaeb7e6e4ae0374f",
+    "url": "/css-gradient-generator/static/css/2.1d5ea050.chunk.css"
+  },
+  {
+    "revision": "57836b4611f4214e8c15",
+    "url": "/css-gradient-generator/static/css/main.54f1b801.chunk.css"
+  },
+  {
+    "revision": "f8b3eaeb7e6e4ae0374f",
+    "url": "/css-gradient-generator/static/js/2.169b4c45.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/css-gradient-generator/static/js/2.169b4c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57836b4611f4214e8c15",
+    "url": "/css-gradient-generator/static/js/main.f99e3460.chunk.js"
+  },
+  {
+    "revision": "d964404cb25ab433f039",
+    "url": "/css-gradient-generator/static/js/runtime-main.1a06b56c.js"
+  }
+]);
