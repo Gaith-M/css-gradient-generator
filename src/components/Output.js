2895036c3a1d4791add0a9968a543807dbebe4ae
Gaith-M <mhdgaith94@gmail.com>
2020-04-26 17:44:40 +0300

    replaced Highlight.js with React-Highlight

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,17 +1,16 @@
 import React from "react";
-import Highlight from "./Highlight";
+import Highlight from "react-highlight";
 import "../App.css";
 
-//Styles for this component are in App.css
 const Output = ({ gradient, gradDegree }) => {
   if (gradient.length <= 1) {
     return <div className="output_styles"></div>;
   }
   return (
     <div className="output_styles">
-      <Highlight
-        content={`<pre class='css'><code>background-image: linear-gradient (${gradDegree}% ${gradient});</code></pre>`}
-      />
+      <Highlight language="css">
+        {`background-image: linear-gradient (${gradDegree}% ${gradient});`}
+      </Highlight>
     </div>
   );
 };
